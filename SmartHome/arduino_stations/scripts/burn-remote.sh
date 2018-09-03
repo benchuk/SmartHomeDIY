71101a46db7b5e139a483ee37eec268a55ff2cbe
benchuknii <benchuk@gmail.com>
2018-09-03 10:43:21 +0300

    adding tasks infra

---
+++ SmartHome/arduino_stations/scripts/burn-remote.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+# osascript -e 'tell app "System Events" to display dialog "Make sure flasher station is connected, then press OK"'
+for i; do 
+    echo $i 
+done 
+avrdude -C "C:\Users\Ben\Downloads\avrdude\etc\avrdude.conf" -v -patmega328p -c arduino -PCOM5 -b115200 -D -U flash:w:text.ino.hex
+
+
+echo "GAME OVER!"
