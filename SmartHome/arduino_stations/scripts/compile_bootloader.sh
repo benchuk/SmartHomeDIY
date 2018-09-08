156345167b77f6e0653ff0084a83e219802d4679
benchuknii <benchuk@gmail.com>
2018-09-08 19:55:04 +0300

    bootloader without watchdog

--- SmartHome/arduino_stations/scripts/compile_bootloader.sh
+++ SmartHome/arduino_stations/scripts/compile_bootloader.sh
@@ -8,4 +8,4 @@ echo -----------------------------------
 cd ..
 cd OTA/bootloader_with_ota_support/optiboot-nrf24l01-master/avr/bootloaders/optiboot-nrf24l01/
 
-make atmega328 LED_START_FLASHES=0 RADIO_UART=1 FORCE_WATCHDOG=0 SUPPORT_EEPROM=1
+make atmega328 LED_START_FLASHES=0 RADIO_UART=1 SUPPORT_EEPROM=1
