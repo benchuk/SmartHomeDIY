3333fb1e607d3981e5388a9e90d0eae140712fd5
Ben Amsallem <benchuk@gmail.com>
2018-09-14 18:38:56 +0300

    touch udpates - t switches on different pin change interrupt groups

--- SmartHome/arduino_stations/touch_switch/project_touch_switch/src/touch_switch_station.ino
+++ SmartHome/arduino_stations/touch_switch/project_touch_switch/src/touch_switch_station.ino
@@ -17,37 +17,67 @@
 #include <nRF24L01.h>
 #include <MirfHardwareSpiDriver.h>
 
-#define rfCLK 13
+#define rfCE 9
+#define rfCS 10
 #define rfMISO 12
 #define rfMOSI 11
-#define D3_PIN_CAHNGE_INTERRUPT 7
+#define rfCLK 13
 
-const int rfCS = 10;
-const int rfCE = 9;
+//pin change interrupts groups are:
+//A0 to A5
+//D0 to D7
+//D8 to D13
+//So using 2 groups pin 7 on one group and pin A3 on the other group
+//This way I do not have to ready pin state to make sure which one was changed.
+#define SWITCH3_PIN_CAHNGE_INTERRUPT 8
+#define SWITCH4_PIN_CAHNGE_INTERRUPT A3
 
 //out to switch relay
-const byte light1Pin = 5;
-const byte light2Pin = 6;
+#define light1Pin 4
+#define light2Pin 5
+#define light3Pin 6
+#define light4Pin 7
 
 //interrupts from touch button
-const byte interruptTouch1Pin = 2;
-const byte interruptTouch2Pin = 3;
+#define interruptTouch1Pin 2
+#define interruptTouch2Pin 3
 
 //swithc state for lights
 volatile byte s1 = HIGH;
 volatile byte s2 = HIGH;
 volatile byte s3 = HIGH;
+volatile byte s4 = HIGH;
+//only needed for pin change swithces - cannot set rais or fall only change
 volatile int s3Counter = 0;
+volatile int s4Counter = 0;
 
 void touch3()
 {
   s3Counter++;
-  if (s3Counter == 2)
+  if (s3Counter == 1)
   {
     s3 = !s3;
-    //digitalWrite(light2Pin, s2);
-    Serial.print("TOUCH3");
+    //digitalWrite(light2Pin, s3);
+    Serial.println("TOUCH3");
+  }
+  if(s3Counter == 2)
+  {
     s3Counter = 0;
+  }
+}
+
+void touch4()
+{
+  s4Counter++;
+  if (s4Counter == 1)
+  {
+    s4 = !s4;
+    //digitalWrite(light2Pin, s4);
+    Serial.println("TOUCH4");
+  }
+  if(s4Counter == 2)
+  {
+    s4Counter = 0;
   }
 }
 
@@ -60,19 +90,26 @@ void setup()
 
   Serial.begin(9600);
 
+  Serial.println("INIT PINS and INTERRUPTS OK");
   pinMode(light1Pin, OUTPUT);
   pinMode(light2Pin, OUTPUT);
-
-  pinMode(interruptTouch1Pin, INPUT_PULLUP);
-  pinMode(interruptTouch2Pin, INPUT_PULLUP);
-
-  attachInterrupt(digitalPinToInterrupt(interruptTouch1Pin), touch1, RISING);
-  attachInterrupt(digitalPinToInterrupt(interruptTouch2Pin), touch2, RISING);
-  pinMode(D3_PIN_CAHNGE_INTERRUPT, INPUT);
-  attachPinChangeInterrupt(D3_PIN_CAHNGE_INTERRUPT, touch3, CHANGE);
+  pinMode(light3Pin, OUTPUT);
+  pinMode(light4Pin, OUTPUT);
 
   digitalWrite(light1Pin, s1);
   digitalWrite(light2Pin, s2);
+  digitalWrite(light3Pin, s3);
+  digitalWrite(light4Pin, s4);
+
+  pinMode(interruptTouch1Pin, INPUT_PULLUP);
+  pinMode(interruptTouch2Pin, INPUT_PULLUP);
+  pinMode(SWITCH3_PIN_CAHNGE_INTERRUPT, INPUT);
+  pinMode(SWITCH4_PIN_CAHNGE_INTERRUPT, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(interruptTouch1Pin), touch1, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptTouch2Pin), touch2, RISING);
+  attachPinChangeInterrupt(SWITCH3_PIN_CAHNGE_INTERRUPT, touch3, CHANGE);
+  attachPinChangeInterrupt(SWITCH4_PIN_CAHNGE_INTERRUPT, touch4, CHANGE);
 
   startRF();
 
@@ -87,7 +124,7 @@ void loop()
   // return;
   while (!Mirf.dataReady())
   {
-    Serial.print(".");
+    //Serial.print(".");
     watchdogReset();
     delay(100);
   };
@@ -147,12 +184,12 @@ void touch1()
 {
   s1 = !s1;
   digitalWrite(light1Pin, s1);
-  Serial.print("TOUCH1");
+  Serial.println("TOUCH1");
 }
 
 void touch2()
 {
   s2 = !s2;
   digitalWrite(light2Pin, s2);
-  Serial.print("TOUCH2");
+  Serial.println("TOUCH2");
 }
