568991f545c0a4342819e97f58db989a75996534
Ben Amsallem <benchuk@gmail.com>
2018-09-23 03:27:05 +0300

    server buttons for touch station  + touch schematics  + touch station bug fixes.

--- SmartHome/arduino_stations/touch_switch/project_touch_switch/src/touch_switch_station.ino
+++ SmartHome/arduino_stations/touch_switch/project_touch_switch/src/touch_switch_station.ino
@@ -17,11 +17,11 @@
 #include <nRF24L01.h>
 #include <MirfHardwareSpiDriver.h>
 
-#define rfCE 9
-#define rfCS 10
-#define rfMISO 12
-#define rfMOSI 11
-#define rfCLK 13
+// #define rfCE 9
+// #define rfCS 10
+// #define rfMISO 12
+// #define rfMOSI 11
+// #define rfCLK 13
 
 //pin change interrupts groups are:
 //A0 to A5
@@ -31,18 +31,22 @@
 //This way I do not have to ready pin state to make sure which one was changed.
 #define SWITCH3_PIN_CAHNGE_INTERRUPT 8
 #define SWITCH4_PIN_CAHNGE_INTERRUPT A3
+//interrupts from touch button
+#define interruptTouch1Pin 2
+#define interruptTouch2Pin 3
 
+
+#define STATUS_LED_PIN A2
 //out to switch relay
 #define light1Pin 4
 #define light2Pin 5
 #define light3Pin 6
 #define light4Pin 7
 
-//interrupts from touch button
-#define interruptTouch1Pin 2
-#define interruptTouch2Pin 3
+
 
 //swithc state for lights
+//RELAY (mechanical ones) are active low - set high so default is lights off
 volatile byte s1 = HIGH;
 volatile byte s2 = HIGH;
 volatile byte s3 = HIGH;
@@ -51,46 +55,86 @@ volatile byte s4 = HIGH;
 volatile int s3Counter = 0;
 volatile int s4Counter = 0;
 
+volatile boolean enabled = false;
+
+unsigned long timestampS1 = 0;
+unsigned long timestampS2 = 0;
+unsigned long timestampS3 = 0;
+unsigned long timestampS4 = 0;
+
+void toogles3()
+{
+  if (millis() - timestampS3 < 500)
+  {
+    return;
+  }
+  timestampS3 = millis();
+  s3 = !s3;
+  digitalWrite(light3Pin, s3);
+}
+
 void touch3()
 {
+  //Serial.println("TOUCH3 ?");
+  if (!enabled)
+  {
+    return;
+  }
   s3Counter++;
   if (s3Counter == 1)
   {
-    s3 = !s3;
-    //digitalWrite(light2Pin, s3);
+    toogles3();
     Serial.println("TOUCH3");
   }
-  if(s3Counter == 2)
+  if (s3Counter == 2)
   {
     s3Counter = 0;
   }
 }
 
+void toogles4()
+{
+  if (millis() - timestampS4 < 500)
+  {
+    return;
+  }
+  timestampS4 = millis();
+  s4 = !s4;
+  digitalWrite(light4Pin, s4);
+}
+
 void touch4()
 {
+  if (!enabled)
+  {
+    return;
+  }
   s4Counter++;
   if (s4Counter == 1)
   {
-    s4 = !s4;
-    //digitalWrite(light2Pin, s4);
+    toogles4();
     Serial.println("TOUCH4");
   }
-  if(s4Counter == 2)
+  if (s4Counter == 2)
   {
     s4Counter = 0;
   }
 }
 
+void blinkReady()
+{
+  digitalWrite(STATUS_LED_PIN, HIGH);
+  delay(50);
+  digitalWrite(STATUS_LED_PIN, LOW);
+  delay(50);
+}
+
 void setup()
 {
-  set_Radio_CE(rfCE);
-  set_Radio_CSN(rfCS);
-
-  configureEEPROMAddressForRFAndOTA("005");
+  configureEEPROMAddressForRFAndOTA("007");
 
   Serial.begin(9600);
 
-  Serial.println("INIT PINS and INTERRUPTS OK");
   pinMode(light1Pin, OUTPUT);
   pinMode(light2Pin, OUTPUT);
   pinMode(light3Pin, OUTPUT);
@@ -101,16 +145,7 @@ void setup()
   digitalWrite(light3Pin, s3);
   digitalWrite(light4Pin, s4);
 
-  pinMode(interruptTouch1Pin, INPUT_PULLUP);
-  pinMode(interruptTouch2Pin, INPUT_PULLUP);
-  pinMode(SWITCH3_PIN_CAHNGE_INTERRUPT, INPUT);
-  pinMode(SWITCH4_PIN_CAHNGE_INTERRUPT, INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(interruptTouch1Pin), touch1, RISING);
-  attachInterrupt(digitalPinToInterrupt(interruptTouch2Pin), touch2, RISING);
-  attachPinChangeInterrupt(SWITCH3_PIN_CAHNGE_INTERRUPT, touch3, CHANGE);
-  attachPinChangeInterrupt(SWITCH4_PIN_CAHNGE_INTERRUPT, touch4, CHANGE);
-
+  Serial.println("Init RF");
   startRF();
 
   Serial.println("INIT OK");
@@ -118,18 +153,49 @@ void setup()
 
 void loop()
 {
+  if (!enabled)
+  {
+    digitalWrite(STATUS_LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(STATUS_LED_PIN, LOW);
+    delay(1000);
+    digitalWrite(STATUS_LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(STATUS_LED_PIN, LOW);
+    delay(1000);
+    digitalWrite(STATUS_LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(STATUS_LED_PIN, LOW);
+
+    Serial.println("INIT PINS and INTERRUPTS");
+    noInterrupts();
+
+    pinMode(interruptTouch1Pin, INPUT_PULLUP);
+    pinMode(interruptTouch2Pin, INPUT_PULLUP);
+    pinMode(SWITCH3_PIN_CAHNGE_INTERRUPT, INPUT);
+    pinMode(SWITCH4_PIN_CAHNGE_INTERRUPT, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(interruptTouch1Pin), touch1, RISING);
+    attachInterrupt(digitalPinToInterrupt(interruptTouch2Pin), touch2, RISING);
+    attachPinChangeInterrupt(SWITCH3_PIN_CAHNGE_INTERRUPT, touch3, CHANGE);
+    attachPinChangeInterrupt(SWITCH4_PIN_CAHNGE_INTERRUPT, touch4, CHANGE);
+    Serial.println("Done init interrupts");
+    enabled = true;
+    interrupts();
+  }
   //  watchdogReset();
-  Serial.print("TICK");
+  //Serial.print("TICK");
+
   //  delay(100);
   // return;
   while (!Mirf.dataReady())
   {
     //Serial.print(".");
     watchdogReset();
-    delay(100);
+    blinkReady();
   };
 
-  Serial.print("TICK");
+  //Serial.print("TICK");
   checkIfOtaRequestOrLoadCommand(cmd);
   Serial.print(F("New command "));
   Serial.println(cmd);
@@ -144,19 +210,20 @@ void loop()
   {
     Serial.print("s1 off");
     s1 = LOW;
+    digitalWrite(light1Pin, s1);
     return;
   }
   else if (strcmp(cmd, "003") == 0)
   {
     Serial.print("s1 on");
     s1 = HIGH;
+    digitalWrite(light1Pin, s1);
     return;
   }
   else if (strcmp(cmd, "004") == 0)
   {
     Serial.print("toogle s1");
-    s1 = !s1;
-    digitalWrite(light1Pin, s1);
+    toogles1();
     return;
   }
   //s2
@@ -164,32 +231,104 @@ void loop()
   {
     Serial.print("s2 off");
     s2 = LOW;
+    digitalWrite(light2Pin, s2);
     return;
   }
   else if (strcmp(cmd, "006") == 0)
   {
     Serial.print("s2 on");
     s2 = HIGH;
+    digitalWrite(light2Pin, s2);
     return;
   }
   else if (strcmp(cmd, "007") == 0)
   {
     Serial.print("toogle s2");
-    s2 = !s2;
+    toogles2();
+    return;
+  }
+  //s3
+  else if (strcmp(cmd, "008") == 0)
+  {
+    Serial.print("s3 off");
+    s3 = LOW;
+    digitalWrite(light3Pin, s3);
+    return;
+  }
+  else if (strcmp(cmd, "009") == 0)
+  {
+    Serial.print("s3 on");
+    s3 = HIGH;
+    digitalWrite(light3Pin, s3);
+    return;
+  }
+  else if (strcmp(cmd, "010") == 0)
+  {
+    Serial.print("toogle s3");
+    toogles3();
+    return;
+  }
+  //s4
+  else if (strcmp(cmd, "011") == 0)
+  {
+    Serial.print("s4 off");
+    s4 = LOW;
+    digitalWrite(light4Pin, s4);
+    return;
+  }
+  else if (strcmp(cmd, "012") == 0)
+  {
+    Serial.print("s4 on");
+    s4 = HIGH;
+    digitalWrite(light4Pin, s4);
+    return;
+  }
+  else if (strcmp(cmd, "013") == 0)
+  {
+    Serial.print("toogle s4");
+    toogles4();
     return;
   }
 }
 
-void touch1()
+void toogles1()
 {
+  if (millis() - timestampS1 < 500)
+  {
+    return;
+  }
+  timestampS1 = millis();
   s1 = !s1;
   digitalWrite(light1Pin, s1);
+}
+
+void touch1()
+{
+  if (!enabled)
+  {
+    return;
+  }
+  toogles1();
   Serial.println("TOUCH1");
+}
+
+void toogles2()
+{
+  if (millis() - timestampS2 < 500)
+  {
+    return;
+  }
+  timestampS2 = millis();
+  s2 = !s2;
+  digitalWrite(light2Pin, s2);
 }
 
 void touch2()
 {
-  s2 = !s2;
-  digitalWrite(light2Pin, s2);
+  if (!enabled)
+  {
+    return;
+  }
+  toogles2();
   Serial.println("TOUCH2");
 }
