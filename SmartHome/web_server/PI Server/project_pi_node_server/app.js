97a3c1958d13bf3f763d65e2c35efa994abf03fc
Ben Amsallem <benchuk@gmail.com>
2018-09-30 23:04:15 +0300

    add apis for ifttt

--- SmartHome/web_server/PI Server/project_pi_node_server/app.js
+++ SmartHome/web_server/PI Server/project_pi_node_server/app.js
@@ -4,9 +4,9 @@
  * Our base app code, including Express configs
  */
 
-var express = require('express')
-    , engine = require('ejs-locals')
-    , app = express();
+var express = require('express'),
+    engine = require('ejs-locals'),
+    app = express();
 
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/ttyAMA0", {
@@ -31,7 +31,10 @@ exports.init = function (port) {
     app.engine('ejs', engine);
 
     app.configure('development', function () {
-        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+        app.use(express.errorHandler({
+            dumpExceptions: true,
+            showStack: true
+        }));
         // app.use(express.logger({ format: ':method :url' }));
     });
 
@@ -41,61 +44,81 @@ exports.init = function (port) {
 
 
     app.use(function (err, req, res, next) {
-        res.render('500.ejs', { locals: { error: err }, status: 500 });
+        res.render('500.ejs', {
+            locals: {
+                error: err
+            },
+            status: 500
+        });
     });
 
     //var router = app.Router();
     app.get('/acon', function (req, res) {
         console.log("ac on request");
-        serialPort.write("1\n", function (err, res) {
-        });
+        serialPort.write("1\n", function (err, res) {});
         res.send('acon');
     });
 
     app.get('/test', function (req, res) {
         console.log("ac on request");
-        serialPort.write("003:005", function (err, res) {
-        });
+        serialPort.write("003:005", function (err, res) {});
         res.send('acon');
     });
 
     //var router = app.Router();
     app.get('/acoff', function (req, res) {
         console.log("ac off request");
-        serialPort.write("2\n", function (err, res) {
-        });
+        serialPort.write("2\n", function (err, res) {});
         res.send('acoff');
     });
 
     //Currently using only this one. smart home api
     app.get('/harq', function (req, res) {
-        try
-        {
-        var value = req.query.value;
-        console.log("harq with value:" + value);
-        serialPort.write(value, function (err, res) {
-        });
-        res.send(value + "sent...");
+        try {
+            var value = req.query.value;
+            console.log("harq with value:" + value);
+            serialPort.write(value, function (err, res) {});
+            res.send(value + "sent...");
+        } catch (err) {
+            console.log(err);
         }
-        catch(err)
-        {
+    });
+
+    app.post('/api/test', function (req, res) {
+        res.send("post test ok");
+    });
+
+
+    app.get('/all-lights-on', function (req, res) {
+        try {
+            handleTimeoutRequest("007:011", 100);
+            handleTimeoutRequest("007:005", 1100);
+            //TBD - add more lighs here
+            res.send("all-lights-on - sent...");
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
+    app.get('/all-lights-off', function (req, res) {
+        try {
+            handleTimeoutRequest("007:012", 100);
+            handleTimeoutRequest("007:006", 1100);
+            //TBD - add more lighs here
+            res.send("all-lighst-off - sent...");
+        } catch (err) {
             console.log(err);
         }
     });
 
 
-    function handleTimeoutRequest(value, time)
-    {
-        var handle = setTimeout(function() {
-            try
-            {
+    function handleTimeoutRequest(value, time) {
+        var handle = setTimeout(function () {
+            try {
                 console.log("Performing now! -> req for time: " + time + " with value: " + value);
-                serialPort.write(value, function (err, res) {
-                });
-           
-            }
-            catch(err)
-            {
+                serialPort.write(value, function (err, res) {});
+
+            } catch (err) {
                 console.log(err);
             }
         }, time);
@@ -106,35 +129,35 @@ exports.init = function (port) {
     var schedualer = {}
     var requestIdCounter = 0;
     app.get('/req-with-at-time', function (req, res) {
-      
+
         var value = req.query.value;
-        var time = req.query.time*1000*60;
+        var time = req.query.time * 1000 * 60;
         console.log("req for time: " + time + " with value:" + value);
-        var handle = handleTimeoutRequest(value,time)
-       
-        schedualer[requestIdCounter] = {handle: handle, timestamp: new Date().getTime()};
+        var handle = handleTimeoutRequest(value, time)
+
+        schedualer[requestIdCounter] = {
+            handle: handle,
+            timestamp: new Date().getTime()
+        };
         //res.send(schedualer[value]);
         res.send(requestIdCounter);
         requestIdCounter++;
     });
 
     app.get('/cancel-req-with-at-time', function (req, res) {
-      
+
         var reqId = req.query.reqId;
-        var time = req.query.time*1000*60;
-        
+        var time = req.query.time * 1000 * 60;
+
         var result = "";
-        try
-        {
+        try {
             result = clearTimeout(schedualer[reqId].handle);
-        }
-        catch(err)
-        {
+        } catch (err) {
             result = err;
         }
 
         res.send(result);
-       
+
     });
 
     server = app.listen(3001);
@@ -148,13 +171,11 @@ serialPort.on("open", function () {
     console.log('open');
     serialPort.on('data', function (data) {
         console.log(data);
-        serialPort.write(data, function (err, res) {
-        });
+        serialPort.write(data, function (err, res) {});
 
 
     });
-    serialPort.write("Server is running and listenning on serial port\n", function (err, res) {
-    });
+    serialPort.write("Server is running and listenning on serial port\n", function (err, res) {});
 });
 
 
