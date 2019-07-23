f5d2553f1166abef30b3460f65d4b7d4da1eb7e7
Ben Amsallem <benchuk@gmail.com>
2019-07-24 00:36:47 +0300

    TV status station updates - working

--- SmartHome/arduino_stations/tv_station/project_tv_station/src/tv_station.cpp
+++ SmartHome/arduino_stations/tv_station/project_tv_station/src/tv_station.cpp
@@ -11,12 +11,12 @@ Payload p;
 uint8_t isTVOn = 1;
 
 void signalState() {
-    Serial.println("signalState");
+  //  Serial.println("signalState");
     p.address = 104;
     p.type = TV_STATION;
     p.data = isTVOn;
-    Serial.println("bin state");
-    Serial.println(p.data, BIN);
+   // Serial.println("bin state");
+    //Serial.println(p.data, BIN);
     // wait for prev send to finish
     while (Mirf.isSending())
         ;
@@ -27,7 +27,21 @@ void signalState() {
     Mirf.send((uint8_t*)(&p));
     while (Mirf.isSending())
         ;
-    Serial.println("Done");
+    //Serial.println("Done");
+}
+
+long readVcc() 
+{
+    long result;
+    // Read 1.1V reference against AVcc
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+    delay(2); // Wait for Vref to settle
+    ADCSRA |= _BV(ADSC); // Convert
+    while (bit_is_set(ADCSRA,ADSC));
+    result = ADCL;
+    result |= ADCH<<8;
+    result = 1126400L / result; // Back-calculate AVcc in mV
+    return result;
 }
 
 void setup() {
@@ -41,13 +55,19 @@ void setup() {
     startRF();
 
     Serial.println("INIT OK TV Station");
+     signalState();
 }
 
 void loop() {
 
-   signalState();
+    long vcc = readVcc();
+    if(vcc < 3500)
+    {
+        isTVOn  = 0;
+         signalState();
+    }
 
- Serial.println("listen....");
-
-  sleepMinutes(20);
+    Serial.println(vcc);
+  //  Serial.println("listen....");
+    //sleepMinutes(20);
 }
