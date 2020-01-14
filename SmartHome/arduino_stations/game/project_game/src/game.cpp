65f260ae93d11b5de28076829686133a74cf37c2
Ben Amsallem <benchuk@gmail.com>
2020-01-14 09:59:15 +0200

    adding switch interrupt support

--- SmartHome/arduino_stations/game/project_game/src/game.cpp
+++ SmartHome/arduino_stations/game/project_game/src/game.cpp
@@ -1,114 +1,27 @@
-/*
-      8x8 LED Matrix MAX7219 Example 01
-
-   by Dejan Nedelkovski, www.HowToMechatronics.com
-
-   Based on the following library:
-   GitHub | riyas-org/max7219  https://github.com/riyas-org/max7219
-*/
-// #include <Arduino.h>
-// #include <MaxMatrix.h>
-
-// int DIN = 7;   // DIN pin of MAX7219 module
-// int CLK = 6;   // CLK pin of MAX7219 module
-// int CS = 5;    // CS pin of MAX7219 module
-// int maxInUse = 1;
-
-// MaxMatrix m(DIN, CS, CLK, maxInUse);
-
-// char A[] = {4, 8,
-//             B01111110,
-//             B00010001,
-//             B00010001,
-//             B01111110,
-//            };
-
-// char B[] = {4, 8,
-//             B01111111,
-//             B01001001,
-//             B01001001,
-//             B00110110,
-//            };
-
-// char smile01[] = {8, 8,
-//                   B00111100,
-//                   B01000010,
-//                   B10010101,
-//                   B10100001,
-//                   B10100001,
-//                   B10010101,
-//                   B01000010,
-//                   B00111100
-//                  };
-// char smile02[] = {8, 8,
-//                   B00111100,
-//                   B01000010,
-//                   B10010101,
-//                   B10010001,
-//                   B10010001,
-//                   B10010101,
-//                   B01000010,
-//                   B00111100
-//                  };
-// char smile03[] = {8, 8,
-//                   B00111100,
-//                   B01000010,
-//                   B10100101,
-//                   B10010001,
-//                   B10010001,
-//                   B10100101,
-//                   B01000010,
-//                   B00111100
-//                  };
-
-// void setup() {
-//   m.init(); // MAX7219 initialization
-//   m.setIntensity(8); // initial led matrix intensity, 0-15
-// }
-
-// void loop() {
-//   // Seting the LEDs On or Off at x,y or row,column position
-//   m.setDot(6,2,true);
-//   delay(1000);
-//   m.setDot(6,3,true);
-//   delay(1000);
-//   m.clear(); // Clears the display
-//   for (int i=0; i<8; i++){
-//     m.setDot(i,i,true);
-//     delay(300);
-//   }
-//   m.clear();
-//   // Displaying the character at x,y (upper left corner of the character)
-//   m.writeSprite(2, 0, A);
-//   delay(1000);
-
-//   m.writeSprite(2, 0, B);
-//   delay(1000);
-
-//   m.writeSprite(0, 0, smile01);
-//   delay(1000);
-
-//   m.writeSprite(0, 0, smile02);
-//   delay(1000);
-
-//   m.writeSprite(0, 0, smile03);
-//   delay(1000);
-
-//   for (int i=0; i<8; i++){
-//     m.shiftLeft(false,false);
-//     delay(300);
-//   }
-//   m.clear();
-
-// }
-
 //***************** 8x8 -> pins 567 **********************//
 //***************** Color strip -> pin 8 **********************//
 //***************** speaker pin 9 **********************//
+//***************** S1 pin 10 **********************//
+//***************** S2 pin A3 **********************//
+//***************** S3 pin 2 **********************//
+//***************** S4 pin 3 **********************//
 
 #include <Arduino.h>
 #include <FastLED.h>
 #include <MaxMatrix.h>
+#include <PinChangeInt.h>
+
+// pin change interrupts groups are:
+// A0 to A5
+// D0 to D7
+// D8 to D13
+// So using 2 groups pin 7 on one group and pin A3 on the other group
+// This way I do not have to ready pin state to make sure which one was changed.
+#define SWITCH3_PIN_CHANGE_INTERRUPT 10
+#define SWITCH4_PIN_CHANGE_INTERRUPT A3
+// interrupts from touch button
+#define interruptTouch1Pin 2
+#define interruptTouch2Pin 3
 
 int DIN = 7; // DIN pin of MAX7219 module
 int CLK = 6; // CLK pin of MAX7219 module
@@ -356,7 +269,26 @@ const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM = {
 //     4, 8, B01111110, B00010001, B00010001, B01111110,
 // };
 
+void onS1() {}
+void onS2() {}
+void onS3() {}
+void onS4() {}
+
+void initInterrupts() {
+    noInterrupts();
+    pinMode(interruptTouch1Pin, INPUT_PULLUP);
+    pinMode(interruptTouch2Pin, INPUT_PULLUP);
+    pinMode(SWITCH3_PIN_CHANGE_INTERRUPT, INPUT);
+    pinMode(SWITCH4_PIN_CHANGE_INTERRUPT, INPUT);
+    attachInterrupt(digitalPinToInterrupt(interruptTouch1Pin), onS1, RISING);
+    attachInterrupt(digitalPinToInterrupt(interruptTouch2Pin), onS2, RISING);
+    attachPinChangeInterrupt(SWITCH3_PIN_CHANGE_INTERRUPT, onS3, CHANGE);
+    attachPinChangeInterrupt(SWITCH4_PIN_CHANGE_INTERRUPT, onS4, CHANGE);
+    interrupts();
+}
+
 void setup() {
+    initInterrupts();
     m.init();          // MAX7219 initialization
     m.setIntensity(8); // initial led matrix intensity, 0-15
 
