10ee42d68497b45ca80dfc870eb9293c8615a195
Ben Amsallem <benchuk@gmail.com>
2020-01-15 01:21:07 +0200

    .

--- SmartHome/arduino_stations/game/project_game/src/game.cpp
+++ SmartHome/arduino_stations/game/project_game/src/game.cpp
@@ -75,12 +75,150 @@ extern CRGBPalette16 myRedWhiteBluePalette;
 extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 
 int speakerOut = 9;
-int melody[] = {C, b, g, C, b, e, R, C, c, g, a, C};
+int melody1[] = {C, b, g, C, b, e, R, C, c, g, a, C};
+int melody2[] = {a, b, C, g, b, R, e, g, C, e, f, a};
 int beats[] = {16, 16, 16, 8, 8, 16, 32, 16, 16, 16, 8, 8};
-int MAX_COUNT = sizeof(melody) / 2; // Melody length, for looping.
+int MAX_COUNT = sizeof(melody1) / 2; // Melody length, for looping.
 
+#define c  261
+#define d 294
+#define e  329
+#define f 349
+#define g 391
+#define gS 415
+#define a  440
+#define aS  455
+#define b  466
+#define cH 523
+#define cSH  554
+#define dH  587
+#define dSH 622
+#define eH  659
+#define fH 698
+#define fSH  740
+#define gH  784
+#define gSH 830
+#define aH 880
+
+void beep(int note, int duration)
+{
+  //Play tone on buzzerPin
+    tone(speakerOut, note, duration);
+
+ //Play different LED depending on value of 'counter'
+  if(counter % 2 == 0)
+  {
+   // digitalWrite(ledPin1, HIGH);
+    delay(duration);
+   // digitalWrite(ledPin1, LOW);
+  }else
+  {
+    //digitalWrite(ledPin2, HIGH);
+    delay(duration);
+    //digitalWrite(ledPin2, LOW);
+  }
+
+  //Stop tone on buzzerPin
+  noTone(speakerOut);
+
+  delay(50);
+
+  //Increment counter
+  counter++;
+
+}
+//C C C C A#  C  C C C
+void ninja()
+{
+
+  beep(cH, 200);
+  beep(cH, 200);
+  beep(cH, 200);
+  beep(cH, 200);
+  //delay(100);
+  beep(aS, 200);
+  delay(270);
+  beep(cH, 200);
+  delay(600);
+ beep(cH, 200);
+  beep(cH, 200);
+  beep(cH, 200);
+
+  delay(500);
+}
+void brawstars()
+{
+    beep(cH, 150);
+  beep(aS, 150);
+  beep(cH, 150);
+  beep(dSH, 150);
+
+  delay(220);
+
+  beep(g, 160);
+  beep(aS,160);
+  beep(cH, 160);
+
+  delay(500);
+
+
+}
+  void firstSection()
+{
+  beep(a, 500);
+  beep(a, 500);
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+
+  delay(500);
+
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+
+  delay(500);
+}
+
+void secondSection()
+{
+  beep(aH, 500);
+  beep(a, 300);
+  beep(a, 150);
+  beep(aH, 500);
+  beep(gSH, 325);
+  beep(gH, 175);
+  beep(fSH, 125);
+  beep(fH, 125);
+  beep(fSH, 250);
+
+  delay(325);
+
+  beep(aS, 250);
+  beep(dSH, 500);
+  beep(dH, 325);
+  beep(cSH, 175);
+  beep(cH, 125);
+  beep(b, 125);
+  beep(cH, 250);
+
+  delay(350);
+}
+
+// change this to make the song slower or faster
+int tempo = 108;
 // Set overall tempo
-long tempo = 10000;
+//long tempo = 10000;
 // Set length of pause between notes
 int pause = 1000;
 // Loop variable to increase Rest length
@@ -116,10 +254,24 @@ void playTone() {
     }
 }
 int playingMusic = 0;
-void playMusic() {
+void playMusic1() {
     playingMusic = 1;
     for (int i = 0; i < MAX_COUNT; i++) {
-        tone_ = melody[i];
+        tone_ = melody1[i];
+        beat = beats[i];
+
+        duration = beat * tempo; // Set up timing
+
+        playTone();
+        // A pause between notes...
+        delayMicroseconds(pause);
+    }
+    playingMusic = 0;
+}
+void playMusic2() {
+    playingMusic = 1;
+    for (int i = 0; i < MAX_COUNT; i++) {
+        tone_ = melody2[i];
         beat = beats[i];
 
         duration = beat * tempo; // Set up timing
@@ -392,6 +544,182 @@ void initState()
     counter = 1;
      gameEnded = 0;
 }
+
+
+
+/*
+  Star Wars theme
+  Connect a piezo buzzer or speaker to pin 11 or select a new pin.
+  More songs available at https://github.com/robsoncouto/arduino-songs
+
+                                              Robson Couto, 2019
+*/
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+
+// // change this to make the song slower or faster
+// int tempo = 108;
+
+// change this to whichever pin you want to use
+int buzzer = 11;
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  // Dart Vader theme (Imperial March) - Star wars
+  // Score available at https://musescore.com/user/202909/scores/1141521
+  // The tenor saxophone part was used
+
+  NOTE_AS4,8, NOTE_AS4,8, NOTE_AS4,8,//1
+  NOTE_F5,2, NOTE_C6,2,
+  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,
+  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,
+  NOTE_AS5,8, NOTE_A5,8, NOTE_AS5,8, NOTE_G5,2, NOTE_C5,8, NOTE_C5,8, NOTE_C5,8,
+  NOTE_F5,2, NOTE_C6,2,
+  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,
+
+  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4, //8
+  NOTE_AS5,8, NOTE_A5,8, NOTE_AS5,8, NOTE_G5,2, NOTE_C5,-8, NOTE_C5,16,
+  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
+  NOTE_F5,8, NOTE_G5,8, NOTE_A5,8, NOTE_G5,4, NOTE_D5,8, NOTE_E5,4,NOTE_C5,-8, NOTE_C5,16,
+  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
+
+  NOTE_C6,-8, NOTE_G5,16, NOTE_G5,2, REST,8, NOTE_C5,8,//13
+  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
+  NOTE_F5,8, NOTE_G5,8, NOTE_A5,8, NOTE_G5,4, NOTE_D5,8, NOTE_E5,4,NOTE_C6,-8, NOTE_C6,16,
+  NOTE_F6,4, NOTE_DS6,8, NOTE_CS6,4, NOTE_C6,8, NOTE_AS5,4, NOTE_GS5,8, NOTE_G5,4, NOTE_F5,8,
+  NOTE_C6,1
+
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+
+void play3()
+{
+    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(speakerOut, melody[thisNote], noteDuration*0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+
+    // stop the waveform generation before the next note.
+    noTone(speakerOut);
+  }
+}
 void setup() {
       Serial.begin(9600);
       pinMode(speakerOut, OUTPUT);
@@ -414,6 +742,9 @@ void setup() {
 }
 
 void loop() {
+//play3();
+//brawstars();
+//ninja();
     if(playingMusic)
 {
     return;
@@ -470,7 +801,9 @@ if(time1 > time2)
      //Serial.println("onS1");
      setAllColor(CRGB::Red);
         m.clear();
-    playMusic();
+   // playMusic1();
+     //firstSection();
+     ninja();
     gameEnded = 1;
     delay(1000);
      return;
@@ -480,7 +813,36 @@ else if(time1 < time2)
      // Serial.println("onS1");
      setAllColor(CRGB::Blue);
         m.clear();
-    playMusic();
+    //playMusic2();
+   // secondSection();
+   //Variant 1
+//   beep(f, 250);
+//   beep(gS, 500);
+//   beep(f, 350);
+//   beep(a, 125);
+//   beep(cH, 500);
+//   beep(a, 375);
+//   beep(cH, 125);
+//   beep(eH, 650);
+
+//   delay(500);
+
+  //Repeat second section
+  //secondSection();
+  brawstars();
+
+//   //Variant 2
+//   beep(f, 250);
+//   beep(gS, 500);
+//   beep(f, 375);
+//   beep(cH, 125);
+//   beep(a, 500);
+//   beep(f, 375);
+//   beep(cH, 125);
+//   beep(a, 650);
+
+//   delay(650);
+
     gameEnded = 1;
     delay(1000);
     return;
